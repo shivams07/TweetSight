68a8da6f1b88199b80282357f90b9351b7ac12de
Shivam Singh <54738220+shivams07@users.noreply.github.com>
2022-11-16 13:54:11 +0530

    Update config.py

--- server/main/config.py
+++ server/main/config.py
@@ -6,10 +6,10 @@ class BaseConfig:
     FLASK_APP="main/__init__.py"
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
     DEBUG = False
-    CONSUMER_KEY = 'CIfavXbvcO8f3ewG2L0H7oGzs'
-    CONSUMER_SECRET = 'EXdKsu0Wg8rndpfkn0TEnCqLeELxUec1l67fj1Aqi4iT1PrafO'
-    ACCESS_TOKEN = '3056320099-U6eA3ZBqeSWcmKGpTD54o8nVxgFeP1d1eGkiLRh'
-    ACCESS_TOKEN_SECRET = 'uovzDBbxrz9wC4y8OUTpzXHMyGh0oGe44pSbkMeAgoXkd'
+    CONSUMER_KEY = ''
+    CONSUMER_SECRET = ''
+    ACCESS_TOKEN = ''
+    ACCESS_TOKEN_SECRET = ''
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration."""
